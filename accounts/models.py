6dd67cf7a41e3bbd62e8931aeed0b1e0d5bc0d25
kenketsu <razgriz.memai02723@gmail.com>
2022-08-21 17:34:02 +0900

    サインアップ機能の実装

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    pass
 
 # class FriendShip(models.Model):
 #     pass
--- mysite/settings.py
+++ mysite/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
@@ -124,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
