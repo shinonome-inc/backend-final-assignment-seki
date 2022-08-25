9e826b84dca73d004c776a4787f7dc3d01859fee
kenketsu <razgriz.memai02723@gmail.com>
2022-08-25 15:51:27 +0900

    ログインログアウト機能の実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "tweets/home.html"
